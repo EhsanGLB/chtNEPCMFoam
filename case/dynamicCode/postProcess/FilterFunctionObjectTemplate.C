5309cde79c9b57a9abe1455ec60c621671b17da2
Ehsan Golab <106700457+EhsanGLB@users.noreply.github.com>
2023-12-26 16:13:13 +0330

    Add files via upload

---
+++ case/dynamicCode/postProcess/FilterFunctionObjectTemplate.C
@@ -0,0 +1,66 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | foam-extend: Open Source CFD
+   \\    /   O peration     | Version:     4.1
+    \\  /    A nd           | Web:         http://www.foam-extend.org
+     \\/     M anipulation  | For copyright notice see file Copyright
+-------------------------------------------------------------------------------
+License
+    This file is part of foam-extend.
+
+    foam-extend is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    foam-extend is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with foam-extend.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "FilterFunctionObjectTemplate.H"
+
+// * * * * * * * * * * * * * * * Global Functions  * * * * * * * * * * * * * //
+
+extern "C"
+{
+    // dynamicCode:
+    // SHA1 = 3d452ce0e764058c4bf076968f631afcf4cad609
+    //
+    // unique function name that can be checked if the correct library version
+    // has been loaded
+    void postProcess_3d452ce0e764058c4bf076968f631afcf4cad609(bool load)
+    {
+        if (load)
+        {
+            // code that can be explicitly executed after loading
+        }
+        else
+        {
+            // code that can be explicitly executed before unloading
+        }
+    }
+}
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    defineNamedTemplateTypeNameAndDebug(postProcessFilterFunctionObject, 0);
+
+    //addToRunTimeSelectionTable
+    addToRunTimeSelectionTable
+    (
+        functionObject,
+        postProcessFilterFunctionObject,
+        dictionary
+    );
+}
+
+// ************************************************************************* //
+
